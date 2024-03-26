b7fa0a5a71da8f518d00f58795763ca10001e4b9
Andy An <andyanappdev@gmail.com>
2024-03-27 01:37:29 +0900

    feat(Conversation): Define Conversation data model
    - A class to hold and manage the conversation from the session

---
+++ ChatBot/ChatBot/Domain/Model/Conversation.swift
@@ -0,0 +1,23 @@
+//
+//  Conversation.swift
+//  ChatBot
+//
+//  Created by Doyoung An on 3/27/24.
+//
+
+class Conversation {
+    private var messages: [Message]
+    
+    init(messages: [Message]) {
+        self.messages = []
+    }
+    
+    func addNewMessage(role: MessageRole = MessageRole.user, content: String) {
+        let newMessage = Message(role: role, content: content)
+        messages.append(newMessage)
+    }
+    
+    func fetchMessages() -> [Message] {
+        return messages
+    }
+}
