bc7a492d89cae9ec368d6942779741771db54ec4
Hoi.Kwon <teaho177@gmail.com>
2024-04-07 17:35:10 +0900

    feat(*): 키보드에 따른 View이동 구현

--- ChatBot/ChatBot/View/DetailChat/DetailChatViewController.swift
+++ ChatBot/ChatBot/View/DetailChat/DetailChatViewController.swift
@@ -28,14 +28,13 @@ final class DetailChatViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        setupKeyboardEvent()
     }
-    
     
     // MARK: - func
     private func configureDetailChatStackView() {
-        
         self.view.addSubview(detailChatStackView)
-        
         detailChatStackView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             detailChatStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
@@ -45,16 +44,30 @@ final class DetailChatViewController: UIViewController {
         ])
     }
     
+   private func setupKeyboardEvent() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    @objc private func keyboardWillShow(_ sender: Notification) {
+        guard let keyboardFrame = sender.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        let keyboardHeight = keyboardFrame.cgRectValue.height
+        if view.frame.origin.y == 0 {
+            view.frame.origin.y -= keyboardHeight
+        }
+    }
+
+    @objc private func keyboardWillHide(_ sender: Notification) {
+        if view.frame.origin.y != 0 {
+            view.frame.origin.y = 0
+        }
+    }
 }
 
-//extension DetailChatViewController: UICollectionViewDataSource {
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        <#code#>
-//    }
-//    
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        <#code#>
-//    }
-//    
-//}
 
